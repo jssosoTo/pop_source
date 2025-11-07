c478bba95dadc30f85b00411b9a019ae60a95e0b
P0PC0RN <2982538245@qq.com>
2025-11-07 16:33:30 +0800

    ✨ feat(组件): 新增错误页面组件

---
+++ src/components/Error.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1 class="mt-24 text-center text-6xl">404 NOT FOUND</h1>
+    <div class="text-center mt-8"><RouterLink to="/" class="pop-btn">Back Home</RouterLink></div>
+</template>
