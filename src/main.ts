112d7dac05b1f8386303bb0bc98a32c5f9b1900e
P0PC0RN <2982538245@qq.com>
2025-11-07 16:36:28 +0800

    ✨ feat(App): 使用翻译插件、Pinia、路由

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia';
+import translation, { translateOptions } from './ultis/i18n.js';
+import router from './router';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app
+    .use(createPinia())
+    .use(translation, translateOptions)
+    .use(router)
+
+app.mount('#app')
