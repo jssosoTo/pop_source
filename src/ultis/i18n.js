5ca0bcb3cab95124e87a91bf61e4006b5fce504d
P0PC0RN <2982538245@qq.com>
2025-11-12 17:50:16 +0800

    📃 docs(翻译模块): 新增中英字段：格言

--- src/ultis/i18n.js
+++ src/ultis/i18n.js
@@ -1,13 +1,21 @@
 export const translateOptions = {
     'zh-cn': {
+        // 导航栏
         logoTitle: 'Pop Source',
         index: '首页',
-        about: '关于'
+        about: '关于',
+
+        // 底部
+        motto: '热爱一切，总会见一面的'
     },
     'en': {
+        // Navigator
         logoTitle: 'Pop Source',
         index: 'Home',
-        about: 'About'
+        about: 'About',
+
+        // footer
+        motto: 'to be or not to be'
     }
 }
 
