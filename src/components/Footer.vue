29529ef34e44b62d3e17f38f5e05436fb60c0a39
P0PC0RN <2982538245@qq.com>
2025-11-12 17:52:08 +0800

    ✨ feat(底部组件): 引入格言

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,12 @@
 <template>
     <footer class="p-12 border-t border-(--box-shadow-color)">
+        <h2 class="text-center font-bold italic uppercase">{{ translation.motto }}</h2>
         <h4 class="text-center">Copyright © 2025 - present P0PC0RN Su</h4>
     </footer>
 </template>
+
+<script setup lang="ts">
+import { inject } from 'vue';
+
+    const translation: any = inject('app-translation');
+</script>
