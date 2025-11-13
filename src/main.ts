ac22e5da0ce1f63def91fe6c12aa7a178c469832
P0PC0RN <2982538245@qq.com>
2025-11-13 15:10:32 +0800

    ✨ feat(依赖): 引入 animate.css 依赖

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import "animate.css";
 import App from './App.vue'
 import { createPinia } from 'pinia';
 import translation, { translateOptions } from './ultis/i18n.js';
