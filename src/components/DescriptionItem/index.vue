1297cbb6a6ed75ff0c5e1a7dc520141e710fdfc7
P0PC0RN <2982538245@qq.com>
2025-11-13 15:09:37 +0800

    ✨ feat(组件): 新增 DescriptionItem 组件

---
+++ src/components/DescriptionItem/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="text-center">
+        <div class="inline-block">
+            <div class="header-title text-left uppercase">{{ title }}</div>
+            <p class="text-left show-link-color" v-html="marked.parse(content)"></p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { marked } from 'marked';
+    defineProps<{
+        title: string;
+        content: string;
+    }>()
+</script>
+
+<style scoped>
+    .header-title {
+        border-left: .24rem solid var(--primary-hover-text-color);
+        padding-left: 1rem;
+        font-size: 1.6rem;
+        line-height: 1;
+        margin-bottom: 1.2rem;
+    }
+</style>
