747d385da4bd846b10715b46fbe02fb663406d78
P0PC0RN <2982538245@qq.com>
2025-11-27 18:38:38 +0800

    ✨ feat(蒙层组件): 新增灰色蒙层组件

---
+++ src/components/Mask/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="fixed top-0 left-0 z-50 w-full h-full bg-(--btn-bg-color)">
+        <div class="relative top-0 left-0 w-full h-full">
+            <slot name="modal"></slot>
+        </div>
+    </div>
+</template>
